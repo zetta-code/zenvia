8f235dfdbcd9bbb48b887c5fb40e73956a41fa7a
Thiago Sousa Santos <thiagos.xsantos@gmail.com>
2021-05-13 17:30:34 -0300

    Refactor Util class

--- src/Util.php
+++ src/Util.php
@@ -1,76 +1,79 @@
 <?php
+
 /**
- * @link      http://github.com/zetta-repo/zenvia for the canonical source repository
+ * @link      https://github.com/zetta-code/zenvia for the canonical source repository
  * @copyright Copyright (c) 2017 Zetta Code
+ * @license   https://github.com/zetta-code/zenvia/blob/master/LICENSE.d
  */
+
+declare(strict_types=1);
 
 namespace Zetta\Zenvia;
 
-use Zetta\Zenvia\Constant\RequestParam;
-use Zetta\Zenvia\Constant\ResponseParam;
-use Zetta\Zenvia\Exception;
-use Zetta\Zenvia\Http\ResponseInterface;
+use Zetta\Zenvia\Contract\SmsInterface;
+use Zetta\Zenvia\Contract\SmsReceivedResponseInterface;
+use Zetta\Zenvia\Contract\SmsRequestInterface;
+use Zetta\Zenvia\Contract\SmsResponseInterface;
+use Zetta\Zenvia\Contract\SmsStatusResponseInterface;
+use Zetta\Zenvia\Enum\RequestParamEnum;
+use Zetta\Zenvia\Enum\ResponseParamEnum;
 use Zetta\Zenvia\Http\SmsReceivedResponse;
-use Zetta\Zenvia\Http\SmsReceivedResponseInterface;
 use Zetta\Zenvia\Http\SmsRequest;
-use Zetta\Zenvia\Http\SmsRequestInterface;
 use Zetta\Zenvia\Http\SmsResponse;
-use Zetta\Zenvia\Http\SmsResponseInterface;
 use Zetta\Zenvia\Http\SmsStatusResponse;
-use Zetta\Zenvia\Http\SmsStatusResponseInterface;
-use Zetta\Zenvia\Model\SmsInterface;
 use Zetta\Zenvia\Model\SmsReceived;
+use Psr\Http\Message\ResponseInterface;
 
 class Util
 {
 
     /**
      * @param SmsInterface $message
-     * @param int $aggregateId
+     * @param string|int|null $aggregateId
      * @return string|null
      */
     public static function smsToJson($message, $aggregateId = null)
     {
         $array = $message->getArrayCopy();
         if ($aggregateId !== null) {
-            $array[RequestParam::AGGREGATE_ID_KEY] = $aggregateId;
+            $array[RequestParamEnum::aggregateId()->value] = $aggregateId;
         }
-        $json = json_encode([RequestParam::SEND_SMS_REQUEST => $array]);
+        $json = json_encode([RequestParamEnum::sendSmsRequest()->value => $array]);
         return $json;
     }
 
     /**
      * @param SmsInterface[] $list
-     * @param int $aggregateId
+     * @param string|int|null $aggregateId
      * @return string|null
      */
     public static function listToJson($list, $aggregateId = null)
     {
         if (is_array($list)) {
             $array = [
-                RequestParam::SEND_SMS_REQUEST_LIST => []
+                RequestParamEnum::sendSmsRequestList()->value => []
             ];
             if ($aggregateId != null) {
-                $array[RequestParam::AGGREGATE_ID_KEY] = $aggregateId;
+                $array[RequestParamEnum::aggregateId()->value] = $aggregateId;
             }
             foreach ($list as $message) {
-                $array[RequestParam::SEND_SMS_REQUEST_LIST][] = $message->getArrayCopy();
+                $array[RequestParamEnum::sendSmsRequestList()->value][] = $message->getArrayCopy();
             }
-            return json_encode([RequestParam::SEND_SMS_MULTI_REQUEST => $array]);
+            return json_encode([RequestParamEnum::sendSmsMultiRequest()->value => $array]);
         }
         return null;
     }
 
     /**
      * @param SmsInterface|array $data
-     * @param string $aggregateId
+     * @param string|int|null $aggregateId
      * @return string
      */
     public static function stringify($data, $aggregateId = null)
     {
         if (is_array($data)) {
             return self::listToJson($data, $aggregateId);
-        } else if ($data instanceof SmsInterface) {
+        } elseif ($data instanceof SmsInterface) {
             return self::smsToJson($data, $aggregateId);
         } else {
             throw new Exception\InvalidArgumentException(sprintf(
@@ -82,23 +85,38 @@ class Util
 
     /**
      * @param ResponseInterface $response
-     * @return SmsResponseInterface|SmsResponseInterface[]|SmsStatusResponseInterface|SmsReceivedResponseInterface
+     * @return mixed
      */
     public static function toSmsResponse($response)
     {
-        $json = json_decode($response->getBody(), true);
+        $json = json_decode($response->getBody()->getContents(), true);
 
         $smsResponse = null;
-        if (isset($json[ResponseParam::SEND_SMS_RESPONSE])) {
-            $smsResponse = static::createSmsResponseFromArray($json[ResponseParam::SEND_SMS_RESPONSE], ResponseParam::SEND_SMS_RESPONSE);
-        } elseif (isset($json[ResponseParam::SEND_SMS_MULTI_RESPONSE])) {
-            $smsResponse = static::createArraySmsResponseFromArray($json[ResponseParam::SEND_SMS_MULTI_RESPONSE], ResponseParam::SEND_SMS_MULTI_RESPONSE);
-        } elseif (isset($json[ResponseParam::GET_SMS_STATUS_RESP])) {
-            $smsResponse = static::createStatusResponseFromArray($json[ResponseParam::GET_SMS_STATUS_RESP], ResponseParam::GET_SMS_STATUS_RESP);
-        } elseif (isset($json[ResponseParam::CANCEL_SMS_RESP])) {
-            $smsResponse = static::createSmsResponseFromArray($json[ResponseParam::CANCEL_SMS_RESP], ResponseParam::CANCEL_SMS_RESP);
-        } elseif (isset($json[ResponseParam::RECEIVED_RESPONSE])) {
-            $smsResponse = static::createSmsReceivedResponseFromArray($json[ResponseParam::RECEIVED_RESPONSE], ResponseParam::RECEIVED_RESPONSE);
+        if (isset($json[ResponseParamEnum::sendSmsResponse()->value])) {
+            $smsResponse = static::createSmsResponseFromArray(
+                $json[ResponseParamEnum::sendSmsResponse()->value],
+                ResponseParamEnum::sendSmsResponse()->label
+            );
+        } elseif (isset($json[ResponseParamEnum::sendSmsMultiResponse()->value])) {
+            $smsResponse = static::createArraySmsResponseFromArray(
+                $json[ResponseParamEnum::sendSmsMultiResponse()->value],
+                ResponseParamEnum::sendSmsMultiResponse()->label
+            );
+        } elseif (isset($json[ResponseParamEnum::getSmsStatusResp()->value])) {
+            $smsResponse = static::createStatusResponseFromArray(
+                $json[ResponseParamEnum::getSmsStatusResp()->value],
+                ResponseParamEnum::getSmsStatusResp()->label
+            );
+        } elseif (isset($json[ResponseParamEnum::cancelSmsResp()->value])) {
+            $smsResponse = static::createSmsResponseFromArray(
+                $json[ResponseParamEnum::cancelSmsResp()->value],
+                ResponseParamEnum::cancelSmsResp()->label
+            );
+        } elseif (isset($json[ResponseParamEnum::receivedResponse()->value])) {
+            $smsResponse = static::createSmsReceivedResponseFromArray(
+                $json[ResponseParamEnum::receivedResponse()->value],
+                ResponseParamEnum::receivedResponse()->label
+            );
         }
         return $smsResponse;
     }
@@ -116,10 +134,16 @@ class Util
         }
 
         $smsRequest = null;
-        if (isset($json[RequestParam::CALLBACK_MT_REQUEST])) {
-            $smsRequest = static::createSmsRequestFromArray($json[RequestParam::CALLBACK_MT_REQUEST], RequestParam::CALLBACK_MT_REQUEST);
-        } elseif (isset($json[RequestParam::CALLBACK_MO_REQUEST])) {
-            $smsRequest = static::createSmsReceivedRequestFromArray($json[RequestParam::CALLBACK_MO_REQUEST], RequestParam::CALLBACK_MO_REQUEST);
+        if (isset($json[RequestParamEnum::callbackMtRequest()->value])) {
+            $smsRequest = static::createSmsRequestFromArray(
+                $json[RequestParamEnum::callbackMtRequest()->value],
+                RequestParamEnum::callbackMtRequest()->label
+            );
+        } elseif (isset($json[RequestParamEnum::callbackMoRequest()->value])) {
+            $smsRequest = static::createSmsReceivedRequestFromArray(
+                $json[RequestParamEnum::callbackMoRequest()->value],
+                RequestParamEnum::callbackMoRequest()->label
+            );
         }
         return $smsRequest;
     }
@@ -137,77 +161,72 @@ class Util
     }
 
     /**
-     * @param array $array
-     * @param string $name
-     * @return SmsResponse
+     * @param array $context
+     * @param string|null $name
+     * @return SmsResponseInterface
      */
-    public static function createSmsResponseFromArray($array, $name = null)
+    public static function createSmsResponseFromArray(array $context = [], ?string $name = null)
     {
-        $response = new SmsResponse($array['statusCode'], $array['statusDescription'], $array['detailCode'], $array['detailDescription']);
-        if ($name !== null) {
+        $response = new SmsResponse(
+            $context['statusCode'],
+            $context['statusDescription'],
+            $context['detailCode'],
+            $context['detailDescription']
+        );
+        if (! empty($name)) {
             $response->setName($name);
         }
         return $response;
     }
 
     /**
-     * @param array $array
-     * @param string $name
-     * @return SmsRequest
+     * @param array $context
+     * @param string|null $name
+     * @return SmsRequestInterface
      */
-    public static function createSmsRequestFromArray($array, $name = null)
+    public static function createSmsRequestFromArray(array $context = [], ?string $name = null)
     {
         $request = new SmsRequest($name);
-        $request->exchangeArray($array);
+        $request->exchangeArray($context);
 
         return $request;
     }
 
     /**
-     * @param array $array
-     * @param string $name
-     * @return SmsResponse[]
+     * @param array $context
+     * @param string|null $name
+     * @return SmsResponseInterface[]
      */
-    public static function createArraySmsResponseFromArray($array, $name = null)
+    public static function createArraySmsResponseFromArray(array $context = [], ?string $name = null)
     {
         $responses = [];
-        foreach ($array[ResponseParam::SEND_SMS_RESPONSE_LIST] as $response) {
+        foreach ($context[ResponseParamEnum::sendSmsResponseList()->value] as $response) {
             $responses[] = static::createSmsResponseFromArray($response, $name);
         }
         return $responses;
     }
 
     /**
-     * @param array $array
-     * @param string $name
-     * @return SmsStatusResponse
+     * @param array $contect
+     * @param string|null $name
+     * @return SmsStatusResponseInterface
      */
-    public static function createStatusResponseFromArray($array, $name = null)
+    public static function createStatusResponseFromArray(array $context = [], ?string $name = null)
     {
-        $response = new SmsStatusResponse($array['statusCode'], $array['statusDescription'], $array['detailCode'], $array['detailDescription']);
-        if ($name !== null) {
+        $response = new SmsStatusResponse(
+            $context['statusCode'],
+            $context['statusDescription'],
+            $context['detailCode'],
+            $context['detailDescription']
+        );
+        if (! empty($name)) {
             $response->setName($name);
         }
-        $response->setId($array['id']);
-        $response->setReceived($array['received']);
-        $response->setShortCode($array['shortcode']);
-        $response->setMobileOperatorName($array['mobileOperatorName']);
-        return $response;
-    }
-
-    /**
-     * @param array $array
-     * @param string $name
-     * @return SmsReceivedResponse
-     */
-    public static function createSmsReceivedResponseFromArray($array, $name = null)
-    {
-        $response = new SmsReceivedResponse($array['statusCode'], $array['statusDescription'], $array['detailCode'], $array['detailDescription']);
-        if ($name !== null) {
-            $response->setName($name);
-        }
-        foreach ($array[ResponseParam::RECEIVED_MESSAGES] as $received) {
-            $response->addReceived(self::createSmsReceivedFromArray($received));
+        $response->setId($context['id']);
+        $response->setReceived($context['received']);
+        $response->setShortCode($context['shortcode']);
+        if (! empty($context['mobileOperatorName'])) {
+            $response->setMobileOperatorName($context['mobileOperatorName']);
         }
         return $response;
     }
@@ -215,7 +234,32 @@ class Util
     /**
      * @param array $array
      * @param string $name
-     * @return SmsRequest
+     * @return SmsReceivedResponseInterface
+     */
+    public static function createSmsReceivedResponseFromArray(array $array = [], string $name = '')
+    {
+        $response = new SmsReceivedResponse(
+            $array['statusCode'],
+            $array['statusDescription'],
+            $array['detailCode'],
+            $array['detailDescription']
+        );
+        if (! empty($name)) {
+            $response->setName($name);
+        }
+        $receiveds = $array[ResponseParamEnum::receivedMessages()->value];
+        if (is_array($receiveds)) {
+            foreach ($receiveds as $received) {
+                $response->addReceived(self::createSmsReceivedFromArray($received));
+            }
+        }
+        return $response;
+    }
+
+    /**
+     * @param array $array
+     * @param string $name
+     * @return SmsRequestInterface
      */
     public static function createSmsReceivedRequestFromArray($array, $name = null)
     {
